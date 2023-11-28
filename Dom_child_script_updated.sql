e2164f477f0669d4b2c9feecdd22759b784274cd
Ravindra Sagar <150906469+ravindra-sagar-razor@users.noreply.github.com>
2023-11-28 20:40:03 +0530

    Update Dom_child_script_updated.sql

--- Dom_child_script_updated.sql
+++ Dom_child_script_updated.sql
@@ -1,4 +1,4 @@
-drop table if exists temp_parent_dom_child_inv_and_action_updated_tester as
+drop table if exists temp_parent_dom_child_inv_and_action_updated_tester
 
 create table temp_parent_dom_child_inv_and_action_updated_tester as
 
